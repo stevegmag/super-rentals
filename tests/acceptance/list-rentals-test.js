e99e5c89d30263c75834e9f1dcf4e92a642513b9
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-08-21 22:09:27 -0400

    added some basic test for routing

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,19 +1,27 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should show rentals as the home page', async function (assert) {
+  test('should show rentals as the home page', async function (assert) {    
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact'); 
   });
-
+/*
   test('should list available rentals.', async function (assert) {
   });
 
@@ -22,4 +30,5 @@ module('Acceptance | list rentals', function(hooks) {
 
   test('should show details for a selected rental', async function (assert) {
   });
+*/
 });
