d9243140d4eb157fd83d85329fa98111d9e8d9bf
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-08-27 00:07:38 -0400

    updated list-rental tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupApplicationTest } from 'ember-qunit';
 
@@ -29,8 +29,13 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 
-  // test('should filter the list of rentals by city.', async function (assert) {
-  // });
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
+  });
 
   // test('should show details for a selected rental', async function (assert) {
   // });
