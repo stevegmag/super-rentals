9e4d2c0eb52a970945e09b70494d711932d3e824
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-08-29 22:38:38 -0400

    updated list rental test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,11 +1,22 @@
+import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { click, visit, currentURL, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupApplicationTest } from 'ember-qunit';
 
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);  
   setupMirage(hooks);
+  
+  hooks.beforeEach(function() {
+    this.owner.register('service:maps', StubMapsService);
+  });
 
   test('should show rentals as the home page', async function (assert) {    
     await visit('/');
